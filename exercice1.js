a1cf3d5b7f7baae5ca895590b32adaaa0f6dd52f
BessonMikael <besson.mikael04@gmail.com>
2020-12-04 10:11:35 +0100

    exo 71

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,29 @@
-let monDiv = .createElement('div');
+
+
+
+let monDiv = document.createElement('div');
 
 document.getElementById('viewport').appendChild(monDiv);
+monDiv.id = "monDiv";
+monDiv.innerHTML = "ce DIV a etait ajoutez en utilisant le pouvoir de JavaScript";
+
+let monDiv2 = document.createElement("div");
+document.getElementById("viewport").appendChild(monDiv2);
+monDiv2.id = "monDiv2";
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = "100px";
+monDiv2.style.height = "100px";
+
+
+
+let span2 =document.getElementById("aSupprimer");
+span2.remove();
+
+
+
+
+
+
 
 
 
